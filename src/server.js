1228f5e83af34a4ddbfa6994647dff87cd2b7810
Marcoant007 <marcoantnovo@gmail.com>
2021-05-03 10:56:44 -0300

    Iniciando repositorio

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const server = express();
+const routes = require("./routes");
+
+//habilitar arquivos statics
+server.use(express.static("public"))
+server.use(routes)
+server.listen(3000, ()=>(console.log("Servidor rodando na porta 3000 ")))
