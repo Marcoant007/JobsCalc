a9fef50deeb1c631281d21d467cd5b6080732482
Marcoant007 <marcoantnovo@gmail.com>
2021-05-13 09:05:12 -0300

    Alterando rotas,part2

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const server = express();
 const routes = require("./routes");
 
+server.set('view engine', 'ejs')
+
+//user req body
+server.use(express.urlencoded({extended: true}))
+
 //habilitar arquivos statics
 server.use(express.static("public"))
 server.use(routes)
